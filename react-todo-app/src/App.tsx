85422fe03e2fe7f704d0e1031df2a3216288b369
임재혁(Harmon) <harmon@eipgrid.com>
2024-03-28 21:02:37 +0900

    react init

---
+++ react-todo-app/src/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi autem culpa, dolores eligendi esse magnam magni molestiae nostrum obcaecati optio porro ratione repellendus saepe sed soluta tenetur unde ut!
+        </div>
+
+        <div>
+            <Button variant="outline">Click me</Button>
+        </div>
+
+    </div>
+  )
+}
+
+export default App
