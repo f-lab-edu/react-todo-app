9fd4e63f3dacdc20ee212a0356ef6dbe135feef2
임재혁(Harmon) <harmon@eipgrid.com>
2024-03-29 22:39:24 +0900

    feat: #1 modify template layout

--- react-todo-app/src/components/TodoTemplate.tsx
+++ react-todo-app/src/components/TodoTemplate.tsx
@@ -1,7 +1,7 @@
 export const TodoTemplate = ({ children }) => {
     return (
-        <div className="w-[430px] h-[932px] m-auto relative">
-            <div className="w-full h-full bg-white m-auto mt-20 rounded-lg shadow-lg p-5">
+        <div className="w-[430px] h-[932px] m-auto relative border border-black">
+            <div className="w-full h-full bg-white m-auto rounded-lg shadow-lg p-5">
                 {children}
             </div>
         </div>
